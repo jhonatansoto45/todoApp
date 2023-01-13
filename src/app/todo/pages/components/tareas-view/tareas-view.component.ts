e1ae8e83e06065d3bdced07ed04b1db72cdd3d87
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2023-01-13 15:06:39 -0500

    Tareas acciones de eliminar, crear

--- src/app/todo/pages/components/tareas-view/tareas-view.component.ts
+++ src/app/todo/pages/components/tareas-view/tareas-view.component.ts
@@ -1,17 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { Tareas } from '../../../interfaces/todo.interface';
 import { TodoService } from '../../../service/todo.service';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-tareas-view',
   templateUrl: './tareas-view.component.html',
   styleUrls: ['./tareas-view.component.scss'],
 })
 export class TareasViewComponent implements OnInit {
+  loading: boolean = false;
   constructor(private todoService: TodoService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.listadoTareas !== null && !this.listadoTareas.length) {
+      this.loading = true;
+      this.stateLocal();
+    }
+  }
 
   get listadoTareas() {
-    return this.todoService.tareas;
+    return this.todoService.dataTareas;
+  }
+
+  completado(tarea: Tareas): void {
+    this.loading = true;
+    setTimeout(() => {
+      tarea.completado = true;
+      this.todoService.updateStateTareas(tarea);
+      this.loading = false;
+    }, 1000);
+  }
+
+  eliminar(tarea: Tareas): void {
+    Swal.fire({
+      title: '¿Esta seguro que desea eliminar la tarea?',
+      showDenyButton: true,
+      confirmButtonText: 'Aceptar',
+      denyButtonText: `Cancelar`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.loading = true;
+        setTimeout(() => {
+          this.todoService.deleteTarea(tarea);
+          this.loading = false;
+          Swal.fire('Actualizado!', '', 'success');
+        }, 1000);
+      } else if (result.isDenied) {
+        Swal.fire('No se efectuaron cambios.', '', 'info');
+        this.loading = false;
+      }
+    });
+  }
+
+  //* LOCALSTORAGE
+  private stateLocal(): void {
+    const savedStorage = JSON.parse(localStorage.getItem('tareas')!);
+    this.todoService.dataTareas = savedStorage;
+    this.loading = false;
   }
 }
--- src/app/todo/service/todo.service.ts
+++ src/app/todo/service/todo.service.ts
@@ -5,23 +5,37 @@ import { Tareas } from '../interfaces/todo.interface';
   providedIn: 'root',
 })
 export class TodoService {
-  tareas: Tareas[] = [
-    {
-      fecha: '2022-01-12',
-      titulo: 'Organizar habitación',
-      descripcion: 'Organizar todo el cuarto.',
-      completado: false,
-    },
-  ];
+  dataTareas: Tareas[] = [];
 
   constructor() {}
 
+  private localStorage(tarea: Tareas): void {
+    this.dataTareas.unshift(tarea);
+    localStorage.setItem('tareas', JSON.stringify(this.dataTareas));
+  }
+
   pushTareas(tarea: Tareas): void {
-    if (!this.tareas.some((item) => item.titulo === tarea.titulo)) {
-      this.tareas.push(tarea);
-      localStorage.setItem('tareas', JSON.stringify(tarea));
-    } else {
-      console.warn('Ya se encuentra la tarea.');
-    }
+    if (this.dataTareas === null) this.dataTareas = []; //* EL LINT ME RECONOCE ESTO COMO NULL ENTONCES LO IGUALO A UN ARRAY
+
+    if (!this.dataTareas.some((item) => item.titulo === tarea.titulo))
+      this.localStorage(tarea);
+    else console.warn('Ya se encuentra la tarea.');
+  }
+
+  updateStateTareas(item: Tareas): void {
+    const index = this.dataTareas.findIndex(
+      (tarea) => tarea.titulo === item.titulo
+    );
+    this.dataTareas.splice(index, 1);
+    this.dataTareas.push(item);
+    localStorage.setItem('tareas', JSON.stringify(this.dataTareas));
+  }
+
+  deleteTarea(item: Tareas): void {
+    const index = this.dataTareas.findIndex(
+      (tarea) => tarea.titulo === item.titulo
+    );
+    this.dataTareas.splice(index, 1);
+    localStorage.setItem('tareas', JSON.stringify(this.dataTareas));
   }
 }
